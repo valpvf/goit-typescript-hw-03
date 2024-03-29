74feaaf2a7b3dc1826165c63efa9ee942c7c07fa
Valerii Pozhydaiev <Valerii.Pozhydaiev@gmail.com>
2024-03-29 23:07:55 +0200

    Class typing exercises

--- src/2.ts
+++ src/2.ts
@@ -16,15 +16,12 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -32,8 +29,16 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
-}
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
 
+    this.salary += 10000;
+  }
+}
+const employee = new Employee("John", "Sales", 5000);
+console.log("employee", employee.getEmployeeDetails());
+
+const manager = new Manager("Jane", "Marketing", 6000);
+console.log("manager", manager);
 
 export {};
